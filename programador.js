35cc801c5fda683ab10eea7708ff809f17d3c821
Alejandro Jarabo Peñas <a.jarabo@alumnos.upm.es>
2019-02-19 02:14:20 +0100

    Emit en programador

--- programador.js
+++ programador.js
@@ -6,22 +6,10 @@ later.date.localTime();
 
 class Programador extends EventEmitter {
 
-  constructor(termostato,config){
+  constructor(config){
     super();
-    this.termostato = termostato;
     this.config = config;
     this.sched;
-  }
-
-  getTemperatura(hora){
-    var index;
-    var i;
-    for (i=0;i<this.config.length;i++){
-      if(this.config[i].hora == hora){
-        index = i;
-      }
-    }
-    return this.config[index].temperatura;
   }
 
   activarProgramador(){
@@ -30,22 +18,10 @@ class Programador extends EventEmitter {
     var i;
     for (i=0;i<this.config.length;i++){
       this.sched[i] = later.parse.text('at ' + this.config[i].hora);
-    }
-    var n;
-    for (n=0;n<this.config.length;n++){
+      var temp = this.config[i].temperatura;
       later.setInterval( () => {
-        var time = new Date();
-        if ((time.getHours()<10) && (time.getMinutes()<10))
-          var hora = '0' + time.getHours() + ':' + '0' + time.getMinutes();
-        else if ((time.getHours()<10) && (time.getMinutes()>=10))
-          var hora = '0' + time.getHours() + ':' + time.getMinutes();
-        else
-          var hora = time.getHours() + ':' + time.getMinutes();
-
-        var temp = this.getTemperatura(hora);
-        console.log('Cambiando temperatura ideal a '+ temp);
-        this.termostato.indicarTemperaturaIdeal(temp);
-      }, this.sched[n]);
+        this.emit('ideal',temp);
+      }, this.sched[i]);
     }
   }
 
