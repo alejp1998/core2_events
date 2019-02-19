80451dc57ca908a1c1db839bca4decae02dd438c
Alejandro Jarabo Peñas <a.jarabo@alumnos.upm.es>
2019-02-19 01:53:49 +0100

    Cambios base

--- practica2.js
+++ practica2.js
@@ -1,4 +1,4 @@
-
+const Programador = require('./programador');
 const Habitacion = require('./habitacion');
 const Climatizador = require('./climatizador');
 const Termostato = require('./termostato');
@@ -13,6 +13,14 @@ const climatizador = new Climatizador(dormitorio);
 // Creamos un Termostato que mira la temperatura de la habitacion:
 const termostato = new Termostato(dormitorio);
 
+//Creamos fichero de configuracion
+configuracion = [{hora: "01:41",temperatura: 22},{hora: "01:42",temperatura: 18},
+{hora: "01:43",temperatura: 20},{ hora: "01:44",temperatura: 23},
+{hora: "01:45",temperatura: 21},{hora: "01:46",temperatura: 17}];
+
+//Creamos objeto Programador
+const programador = new Programador(termostato,configuracion);
+
 // Configuramos el termostato para controlar la temperatura:
 termostato.on('muchofrio', () => climatizador.calentar());
 termostato.on('muchocalor', () => climatizador.enfriar());
@@ -26,3 +34,5 @@ termostato.indicarTemperaturaIdeal(20);
 // Encender el termostato:
 termostato.encender();
 
+//Activamos el Programador
+programador.activarProgramador();
